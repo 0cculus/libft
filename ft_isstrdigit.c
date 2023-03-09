67077021d903f48d855d0a8f29c74589876d168f
Brian Rheaume-Michaud <brheaume@c2r5p9.42quebec.com>
2023-03-09 13:29:46 -0500

    fixed norminette isstrdigit

--- ft_isstrdigit.c
+++ ft_isstrdigit.c
@@ -6,7 +6,7 @@
 /*   By: brheaume <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 13:02:41 by brheaume          #+#    #+#             */
-/*   Updated: 2023/03/09 13:12:10 by brheaume         ###   ########.fr       */
+/*   Updated: 2023/03/09 13:28:52 by brheaume         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_isstrdigit(const char *src)
 	i = 0;
 	while (src[i])
 	{
-		if(!(src[i] <= '9' && src[i] >= '0'))
+		if (!(src[i] <= '9' && src[i] >= '0'))
 			return (0);
 		i++;
 	}
